5d132619d542affe5eaf5fcef559adae48e3bdd0
MuhammadAflah <112074732+MuhammadAflah@users.noreply.github.com>
2022-09-05 21:45:52 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// $('#playButton').click(function () {
-//     $('#homeCarousel').carousel('cycle');
-// });
-// $('#pauseButton').click(function () {
-//     $('#homeCarousel').carousel('pause');
-// });
